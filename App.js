8b8b72efd1b040b79d83b0d23cd913f80963bc50
Zakariae ISMAILI <zakariae.ismaili@babelgroup.com>
2023-11-05 20:49:35 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Feather } from "@expo/vector-icons";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleLogin = () => {
+    console.log(`Email: ${email}, Password: ${password}`);
+    if(email==="Test@gmail.com" && password==="123") {
+      console.log("Connected");
+    } else {
+      console.log("Wrong credentials! Retry")
+    }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+    console.log(showPassword)
+  };
+
+    return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={styles.content}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Login</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <View style={styles.icon}>
+              <Feather name="mail" size={22} color="#7C8080" />
+            </View>
+            <TextInput
+              style={styles.input}
+              placeholder="E-mail"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <View style={styles.icon}>
+              <Feather name="lock" size={22} color="#7C8080" />
+            </View>
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={!showPassword}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TouchableOpacity style={styles.passwordVisibleButton} onPress={handleShowPassword}>
+              <Feather name="eye-off" size={20} color="#7C8080" />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.forgotPassword}>
+            <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <View>
+            <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+              <Text style={styles.loginButtonText}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 40,
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  forgotPassword: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 20,
+    color: "#3662AA",
+  },
+  forgotPasswordText: {
+    alignSelf: "flex-end",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+    position: "relative",
+  },
+  icon: {
+    marginRight: 15,
+  },
+  input: {
+    borderBottomWidth: 1.5,
+    flex: 1,
+    paddingBottom: 10,
+    borderBottomColor: "#eee",
+    fontSize: 16,
+  },
+  passwordVisibleButton: {
+    position: "absolute",
+    right: 0,
+    color: "#3662AA",
+  },
+  loginButton: {
+    borderRadius: 15,
+    backgroundColor: "#3662AA",
+    padding: 15,
+    alignItems: "center",
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
